9328344f45abce70fb7b6f2149dda0405c558513
Tolga Kağan Aysu <tolga.kagan@hotmail.com>
2021-02-19 00:36:30 +0300

    Initial Commit

---
+++ Köstebek Yakala/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Köstebek Yakala
+//
+//  Created by Tolga Kağan Aysu on 19.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
